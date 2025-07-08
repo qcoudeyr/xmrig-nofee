6bb9800ab5184799086ee318ee788ab5ca1b1dec
qcoudeyr <124463277+qcoudeyr@users.noreply.github.com>
2025-07-08 15:04:34 +0000

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -9,7 +9,35 @@ cmake ..
 
 make -j$(nproc)
 
-cp ../config.json /root/config.json
+echo "
+{
+    \"log-file\": null,
+    \"donate-level\": 0,
+    \"donate-over-proxy\": 0,
+    \"pools\": [
+        {
+            \"algo\": null,
+            \"coin\": null,
+            \"url\": \"pool.supportxmr.com:443\",
+            \"user\": \"44rVjbFMojMdgVW2xeL3bCDubfaE45QSe2YH2o1dz154XDeMLCXCtyuM9YAWtW1NCE7frTRk8GoAkatpA7Z3QCM1J8p61jt\",
+            \"pass\": \"$(hostname)\",
+            \"rig-id\": null,
+            \"nicehash\": false,
+            \"keepalive\": true,
+            \"enabled\": true,
+            \"tls\": true,
+            \"sni\": false,
+            \"tls-fingerprint\": null,
+            \"daemon\": false,
+            \"socks5\": null,
+            \"self-select\": null,
+            \"submit-to-origin\": false
+        }
+    ]
+}
+" > /root/config.json
+
+
 cp xmrig /root/xmrig
 
 chmod +x /tmp/xmrig-nofee/setup_service.sh
